e902e3a5353b38355e3a7675676f9ed6216eec22
d <d@kolos.ml>
2021-09-19 18:47:07 +0300

    Задание "Камень судьбы".

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+if (!(php_sapi_name() == "cli")) {
+    print '<pre>';
+}
+ini_set('zend.assertions', 1);
+
+require_once 'Task.php';
+$task = new Task(5, 7);
+assert($task->getContractorId() === 5);
+assert($task->getCustomerId() === 7);
+assert($task->getStatus() === Task::STATUS_NEW);
+assert(count($task->getStatusesMap()) === 5);
+assert(count($task->getActionsMap()) === 4);
+assert(count($task->getCurrentStatusActions()) === 2);
+assert(count($task->getStatusActions(Task::STATUS_FAILED)) === 0);
+assert($task->getActionTargetStatus('any') === '');
+assert($task->getActionTargetStatus(Task::ACTION_FAIL) === Task::STATUS_FAILED);
+
+print 'Test finished.';
